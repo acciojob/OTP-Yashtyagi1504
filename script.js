fa803f228ae7656c08c0e54f2c795701ed016cbd
Yashtyagi1504 <166196060+Yashtyagi1504@users.noreply.github.com>
2025-02-19 23:42:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,63 +1,108 @@
-// input.addEventListener("focus", ()=>{
- //        input.classList.add("focused");
- //    });
+// // input.addEventListener("focus", ()=>{
+//  //        input.classList.add("focused");
+//  //    });
 
- //    input.addEventListener("blur", () => {
- //        input.classList.remove("focused");
- //    });
+//  //    input.addEventListener("blur", () => {
+//  //        input.classList.remove("focused");
+//  //    });
 
+
+// let inputs = document.querySelectorAll("input");
+
+// inputs.forEach((input,index) => {
+// 	input.addEventListener("input",function(){
+// 		shiftFocus(index);
+// 	})
+// 	input.addEventListener("keydown", function(e){
+// 		unshiftFocus(e,index);
+// 	})
+// 	input.addEventListener("focus", ()=>{
+//         setTimeout(() => {
+//             input.classList.add("focused");
+//         }, 10);
+//     });
+
+//     input.addEventListener("blur", () => {
+//         input.classList.remove("focused");
+//     });
+// })
+
+// function shiftFocus(index){
+// 	if (inputs[index].value !== "" && index<inputs.length - 1) {
+//         inputs[index + 1].focus(); 
+
+		
+// 		const currentInput = inputs[index];
+//     const nextInput = inputs[index + 1];
+    
+//     // Explicitly manage classes
+//     currentInput.classList.remove("focused");
+//     // nextInput.focus();
+//     nextInput.classList.add("focused");
+
+//     }
+// }
+// function unshiftFocus(e,index){
+
+// 	if(e.key === "Backspace" && index>0){
+// 		e.preventDefault();
+// 		inputs[index].value="";
+// 		inputs[index-1].focus();
+
+
+// 		 const currentInput = inputs[index];
+//     const prevInput = inputs[index - 1];
+    
+//     // currentInput.value = "";
+//     currentInput.classList.remove("focused");
+//     // prevInput.focus();
+//     prevInput.classList.add("focused");
+// 	}
+// }
 
 let inputs = document.querySelectorAll("input");
 
-inputs.forEach((input,index) => {
-	input.addEventListener("input",function(){
-		shiftFocus(index);
-	})
-	input.addEventListener("keydown", function(e){
-		unshiftFocus(e,index);
-	})
-	input.addEventListener("focus", ()=>{
-        setTimeout(() => {
-            input.classList.add("focused");
-        }, 10);
-    });
+inputs.forEach((input, index) => {
+  input.addEventListener("input", function() {
+    shiftFocus(index);
+  });
 
-    input.addEventListener("blur", () => {
-        input.classList.remove("focused");
-    });
-})
+  input.addEventListener("keydown", function(e) {
+    unshiftFocus(e, index);
+  });
 
-function shiftFocus(index){
-	if (inputs[index].value !== "" && index<inputs.length - 1) {
-        inputs[index + 1].focus(); 
+  input.addEventListener("focus", () => {
+    input.classList.add("focused");
+  });
 
-		
-		const currentInput = inputs[index];
+  input.addEventListener("blur", () => {
+    input.classList.remove("focused");
+  });
+});
+
+function shiftFocus(index) {
+  if (inputs[index].value !== "" && index < inputs.length - 1) {
+    const currentInput = inputs[index];
     const nextInput = inputs[index + 1];
     
     // Explicitly manage classes
     currentInput.classList.remove("focused");
-    // nextInput.focus();
+    nextInput.focus();
     nextInput.classList.add("focused");
-
-    }
+  }
 }
-function unshiftFocus(e,index){
 
-	if(e.key === "Backspace" && index>0){
-		e.preventDefault();
-		inputs[index].value="";
-		inputs[index-1].focus();
-
-
-		 const currentInput = inputs[index];
+function unshiftFocus(e, index) {
+  if (e.key === "Backspace" && index > 0) {
+    e.preventDefault();
+    const currentInput = inputs[index];
     const prevInput = inputs[index - 1];
     
-    // currentInput.value = "";
+    currentInput.value = "";
     currentInput.classList.remove("focused");
-    // prevInput.focus();
+    prevInput.focus();
     prevInput.classList.add("focused");
-	}
+  }
 }
 
 
